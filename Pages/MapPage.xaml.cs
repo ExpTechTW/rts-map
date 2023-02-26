43b18df0b4658611e04e270db06bf6a34b7bba21
Kamiya <58339640+kamiya10@users.noreply.github.com>
2023-02-26 10:53:39 +0800

    feat: cache and center map

--- Pages/MapPage.xaml.cs
+++ Pages/MapPage.xaml.cs
@@ -3,6 +3,11 @@
 
 using Microsoft.UI.Xaml.Controls;
 using Mapsui.Tiling;
+using Mapsui;
+using Microsoft.UI.Xaml.Navigation;
+using Mapsui.Projections;
+using Mapsui.Extensions;
+using Mapsui.UI.WinUI;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -14,11 +19,22 @@ namespace rts_map.Pages
     /// </summary>
     public sealed partial class MapPage : Page
     {
+        private static Map _map;
+
         public MapPage()
         {
             this.InitializeComponent();
 
-            MyMap.Map.Layers.Add(OpenStreetMap.CreateTileLayer());
+            if (_map == null)
+            {
+                _map = new Map();
+                _map.Layers.Add(OpenStreetMap.CreateTileLayer());
+                var tw = new MPoint(121, 23.5);
+                var coord = SphericalMercator.FromLonLat(tw.X, tw.Y).ToMPoint();
+                _map.Home = n => n.NavigateTo(coord, _map.Resolutions[7]);
+            }
+
+            MyMap.Map = _map;
         }
     }
 }
