6761d98064184a8a663fd732e5313472b40d1700
Kamiya <58339640+kamiya10@users.noreply.github.com>
2024-04-11 17:19:40 +0800

    feat: vue

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-view v-slot="{ Component }">
+    <transition name="fade">
+      <keep-alive>
+        <component :is="Component" />
+      </keep-alive>
+    </transition>
+  </router-view>
+</template>
+
+<script setup>
+console.log('👋 This message is being logged by "App.vue", included via Vite');
+</script>
---
+++ src/main.ts
@@ -0,0 +1,38 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false,
+    backgroundMaterial: "acrylic",
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  } else {
+    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+  }
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
